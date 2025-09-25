ac4153e958649274b92be7632edb5ffddb170976
Sakindu Weenath <121110518+weenaath@users.noreply.github.com>
2025-09-26 00:18:19 +0530

    add initial blog posts data to blogPosts.js

---
+++ src/data/blogPosts.js
@@ -0,0 +1,20 @@
+const blogPosts = [
+  {
+    id: 1,
+    title: "My First Blog Post",
+    date: "2025-09-23",
+    author: "Sakindu",
+    content: "This is the content of my very first blog post...",
+    tags: ["react", "beginner"]
+  },
+  {
+    id: 2,
+    title: "Learning React is Fun!",
+    date: "2025-09-22",
+    author: "Sakindu",
+    content: "Today I learned how to use components in React. It's super cool!",
+    tags: ["react", "webdev"]
+  }
+];
+
+export default blogPosts;
